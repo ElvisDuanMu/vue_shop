087067b6a03c366d0a810d34592c802465d11ac8
ElvisDuanMu <1044682866@qq.com>
2020-09-13 23:43:56 +0800

    提交登录页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,51 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login'
+import Home from '../components/Home'
 
 Vue.use(VueRouter)
 
 const routes = [
+  // 自定义重定向规则
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
 ]
 
 const router = new VueRouter({
+  // 不显示'#'
+  model: 'history',
   routes
+})
+
+// 挂载路由导航守卫，判断是否有权限进入某些特定页面
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from代表从哪个路径跳转过来
+  // next是一个函数，表示放行。 next()：放行    next('/login')：强制跳转
+
+  // 1.登录页面不需要做权限控制
+  if (to.path === '/login') {
+    return next()
+  }
+  // 2.有权限的页面需要拿到token，根据有无token再决定是否放行
+  //   2.1 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  //   2.2 如果token不存在，强制跳转到登录页
+  if (!tokenStr) {
+    return next('/login')
+  }
+  //  2.3 如果token存在，直接放行
+  next()
 })
 
 export default router
