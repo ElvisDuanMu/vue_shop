c9d274794cb1259b431445af6edf6ddf29255d31
ElvisDuanMu <1044682866@qq.com>
2020-09-13 23:26:55 +0800

    tianjia

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    logout () {
+      // 清空sessionStorage
+      window.sessionStorage.removeItem('token')
+      // 跳转到登录页
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
